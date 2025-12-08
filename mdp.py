a0cc754fd66556c6e1ab8bd6f74278b837c7db3c
Solal Jourdan <solal.jourdan@laposte.net>
2025-12-08 15:06:45 +0100

    initial commit with MC and MDP simulations

---
+++ mdp.py
@@ -0,0 +1,68 @@
+from antlr4 import *
+from gramLexer import gramLexer
+from gramListener import gramListener
+from gramParser import gramParser
+from markov import MarkovChain
+import sys
+
+
+class gramPrintListener(gramListener):
+
+    def __init__(self):
+        self.trans = {}
+        self.trans_noact = {}
+
+
+    def enterStatesNoRew(self, ctx):
+        self.states = [str(x) for x in ctx.ID()]
+
+        self.rewards =None
+        print("States: %s" % str(self.states))
+
+    def enterStatesRew(self, ctx):
+        self.states = [str(x) for x in ctx.ID()]
+        ints=[int(str(x)) for x in ctx.INT()]
+
+        self.rewards=ints
+        print("States: %s" % str(self.states))
+        print("Rewards: %s" % str(self.rewards))
+    
+
+    def enterDefactions(self, ctx):
+        self.actions = [str(x) for x in ctx.ID()]
+        print("Actions: %s" % str(self.actions))
+
+    def enterTransact(self, ctx):
+        ids = [str(x) for x in ctx.ID()]
+        dep = ids.pop(0)
+        act = ids.pop(0)
+        weights = [int(str(x)) for x in ctx.INT()]
+        self.trans[act] = [(dep, ids[i], weights[i]) for i in range(len(ids))]
+        print("Transition from " + dep + " with action "+ act + " and targets " + str(ids) + " with weights " + str(weights))
+
+    def enterTransnoact(self, ctx):
+        ids = [str(x) for x in ctx.ID()]
+        dep = ids.pop(0)
+        weights = [int(str(x)) for x in ctx.INT()]
+        self.trans[""] = [(dep, ids[i], weights[i]) for i in range(len(ids))]
+        print("Transition from " + dep + " with no action and targets " + str(ids) + " with weights " + str(weights))
+
+
+
+def main():
+    input_file=sys.argv[1]
+    lexer = gramLexer(FileStream(input_file, encoding='utf-8'))
+    stream = CommonTokenStream(lexer)
+    parser = gramParser(stream)
+    tree = parser.program()
+    printer = gramPrintListener()
+    walker = ParseTreeWalker()
+    walker.walk(printer, tree)
+    markov=MarkovChain(list_states=printer.states, list_actions= printer.actions, list_rewards= printer.rewards, dict_trans=printer.trans)
+    markov.__repr__()
+    print(markov.chain)
+    a,b,c,d=markov.simulation_MDP(10)
+    print(a,b,c,d)
+
+if __name__ == '__main__':
+    main()
